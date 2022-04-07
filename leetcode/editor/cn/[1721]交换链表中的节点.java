3d38271428f635ce672ff155f89ec200283cf0d0
befallenStar <sy5622_5@126.com>
2022-04-07 15:08:31 +0800

    leetcode 1721

---
+++ leetcode/editor/cn/[1721]交换链表中的节点.java
@@ -0,0 +1,80 @@
+//给你链表的头节点 head 和一个整数 k 。 
+//
+// 交换 链表正数第 k 个节点和倒数第 k 个节点的值后，返回链表的头节点（链表 从 1 开始索引）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], k = 2
+//输出：[1,4,3,2,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [7,9,6,6,7,8,3,0,9,5], k = 5
+//输出：[7,9,6,6,8,7,3,0,9,5]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1], k = 1
+//输出：[1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：head = [1,2], k = 1
+//输出：[2,1]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：head = [1,2,3], k = 2
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目是 n 
+// 1 <= k <= n <= 10⁵ 
+// 0 <= Node.val <= 100 
+// 
+// Related Topics 链表 双指针 👍 50 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for singly-linked list. public class ListNode { int val; ListNode next; ListNode() {}
+ * ListNode(int val) { this.val = val; } ListNode(int val, ListNode next) { this.val = val;
+ * this.next = next; } }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode left = head;
+        ListNode right = head;
+        while (k > 1) {
+            right = right.next;
+            k--;
+        }
+        ListNode start = right;
+        while (right.next != null) {
+            left = left.next;
+            right = right.next;
+        }
+        int tmp = left.val;
+        left.val = start.val;
+        start.val = tmp;
+        return head;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
