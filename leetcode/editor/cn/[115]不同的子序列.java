e0265ba6f08be7e669c7372db4814d8619d77160
befallenStar <sy5622_5@126.com>
2022-11-17 11:10:10 +0800

    leetcode 115

---
+++ leetcode/editor/cn/[115]不同的子序列.java
@@ -0,0 +1,68 @@
+//给定一个字符串 s 和一个字符串 t ，计算在 s 的子序列中 t 出现的个数。 
+//
+// 字符串的一个 子序列 是指，通过删除一些（也可以不删除）字符且不干扰剩余字符相对位置所组成的新字符串。（例如，"ACE" 是 "ABCDE" 的一个子序列
+//，而 "AEC" 不是） 
+//
+// 题目数据保证答案符合 32 位带符号整数范围。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "rabbbit", t = "rabbit"
+//输出：3
+//解释：
+//如下图所示, 有 3 种可以从 s 中得到 "rabbit" 的方案。
+//rabbbit
+//rabbbit
+//rabbbit 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "babgbag", t = "bag"
+//输出：5
+//解释：
+//如下图所示, 有 5 种可以从 s 中得到 "bag" 的方案。 
+//babgbag
+//babgbag
+//babgbag
+//babgbag
+//babgbag
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= s.length, t.length <= 1000 
+// s 和 t 由英文字母组成 
+// 
+//
+// Related Topics 字符串 动态规划 👍 899 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        if (m < n) return 0;
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; ++i)
+            dp[i][n] = 1;
+        for (int i = m - 1; i >= 0; --i) {
+            char schar = s.charAt(i);
+            for (int j = n - 1; j >= 0; --j) {
+                char tchar = t.charAt(j);
+                if (schar == tchar)
+                    dp[i][j] = dp[i + 1][j + 1] + dp[i + 1][j];
+                else
+                    dp[i][j] = dp[i + 1][j];
+            }
+        }
+        return dp[0][0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
